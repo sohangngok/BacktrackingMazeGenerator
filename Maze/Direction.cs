e7d98aab124b6f61142cfa4930766ad0433be1a1
sohangngok <sohangngok@gmail.com>
2019-06-05 12:32:49 +0800

    Add project files.

---
+++ Maze/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maze
+{
+    public enum Direction
+    {
+        N,
+        S,
+        E,
+        W
+    }
+}
---
+++ MazeGenTest/MazeGenTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MazeGenTest
+{
+    [TestClass]
+    public class MazeGenTest
+    {
+        [TestMethod]
+        public void GenMazeTest()
+        {
+
+        }
+    }
+}
