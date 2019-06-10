effbbb9213486299a770132b279c82adc38d0864
So Hang Ngok <sohangngok@live.hk>
2019-06-10 20:27:34 +0800

    cell

--- Maze/Cell.cs
+++ Maze/Cell.cs
@@ -11,5 +11,29 @@ namespace Maze
         public Cell CellS { get; set; }
         public Cell CellW { get; set; }
         public Cell CellE { get; set; }
+
+        public Cell(Cell cellN, Cell cellS, Cell cellW, Cell cellE)
+        {
+            CellN = cellN;
+            CellS = cellS;
+            CellW = cellW;
+            CellE = cellE;
+        }
+
+        public Cell()
+        {
+            CellN = null;
+            CellS = null;
+            CellW = null;
+            CellE = null;
+        }
+
+        public void SetNeighbors(Cell cellN, Cell cellS, Cell cellW, Cell cellE)
+        {
+            CellN = cellN;
+            CellS = cellS;
+            CellW = cellW;
+            CellE = cellE;
+        }
     }
 }
--- Maze/RectangularMaze.cs
+++ Maze/RectangularMaze.cs
@@ -8,12 +8,38 @@ namespace Maze
     {
         public readonly int Width;
         public readonly int Height;
-        public Cell cells { get; }
+        /// <summary>
+        /// Grid on an (x, y) coordinate plane. Top left is the origin.
+        /// x is row. y is column.
+        /// </summary>
+        public Cell[,] Cells { get; }
 
         public RectangularMaze(int width, int height)
         {
-            this.Width = width;
-            this.Height = height;
+            Width = width;
+            Height = height;
+            //init cells
+            Cells = new Cell[width, height];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Cells[i, j] = new Cell();
+                }
+            }
+            Cells[0, 0].SetNeighbors(null, Cells[1, 0], null, Cells[0, 1]);
+            Cells[0, width - 1].SetNeighbors(null, Cells[1, width - 1], Cells[0, width - 2], null);
+            Cells[height - 1, 0].SetNeighbors(Cells[height - 2, 0], null, null, Cells[height - 1, 1]);
+            Cells[height - 1, width - 1].SetNeighbors(Cells[height - 2, width - 1], null, Cells[height - 1, width - 2], null);
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    Cells[row, col].CellN = row - 1 > 0 ? Cells[row - 1, col] : null;
+                    //Cells[row, col].CellS = row + 1 <
+                }
+            }
         }
 
         public IMaze GenerateMaze(Random random)
